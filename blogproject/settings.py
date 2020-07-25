0e7eeef03872275ef6aada9f4d946164172d9aa6
kimbaleum <kimbaleum@likelion.org>
2020-07-26 00:07:29 +0900

    third message

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -156,4 +157,6 @@ LOGIN_REDIRECT_URL = '/'
 
 #자동로그아웃
 # ACCOUNT_LOGOUT_ON_GET = True
-
+import dj_database_url 
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
